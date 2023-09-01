e8742e307dc5085bae630024f5638312e764ee9c
Thanh Loi <you@example.com>
2023-09-01 09:07:23 +0700

    Banner fix carousel

--- resources/views/clients/parts/banner.blade.php
+++ resources/views/clients/parts/banner.blade.php
@@ -2,28 +2,41 @@
     <div class="container-md">
         <div class="row align-content-center banner_row">
             <div class="col-md-9 product_banner_wrapper">
-                <div class="product_banner text-white rounded-4 overflow-hidden">
+                {{-- <div
+                    class="product_banner text-white rounded-4 overflow-hidden">
                     <div class="product_wallpaper">
                         <div class="product_banner_body">
-                            <div class="product_banner_title">
+                            <div
+                                class="product_banner_title">
                                 <img class="img-fluid"
                                     src="https://cdn2.unrealengine.com/egs-ac-mirage-carousel-logo-350x143-c22cafeefdfb.png"
                                     alt="bannertitle">
                             </div>
                             <div class="product_publish">
-                                <span>AVAILABLE OCTOBER 5</span>
+                                <span>AVAILABLE OCTOBER
+                                    5</span>
                             </div>
-                            <div class="product_description">
-                                <span>Experience the story of Basim seeking answers and justice as he navigates the
+                            <div
+                                class="product_description">
+                                <span>Experience the story
+                                    of Basim seeking answers
+                                    and justice as he
+                                    navigates the
                                     bustling
-                                    streets of ninth-century Baghdad.</span>
+                                    streets of ninth-century
+                                    Baghdad.</span>
                             </div>
                             <div class="product_actions">
-                                <div class="btn btn-light btn-lg product_actions_buy">
+                                <div
+                                    class="btn btn-light btn-lg product_actions_buy">
                                     PRE-PURCHASE NOW
                                 </div>
-                                <div class="btn bg-transparent btn-lg product_actions_addcart">
-                                    <div class="icon-plus"><i class="fa-solid fa-plus"></i></div>
+                                <div
+                                    class="btn bg-transparent btn-lg product_actions_addcart">
+                                    <div class="icon-plus">
+                                        <i
+                                            class="fa-solid fa-plus"></i>
+                                    </div>
                                     <span>ADD TO CART</span>
                                 </div>
                             </div>
@@ -31,6 +44,238 @@
                     </div>
 
 
+                </div> --}}
+                <div id="carouselExampleCaptions"
+                    class="carousel slide"
+                    data-bs-ride="carousel">
+                    <div class="carousel-indicators">
+                        <button type="button"
+                            data-bs-target="#carouselExampleCaptions"
+                            data-bs-slide-to="0"
+                            class="active"
+                            aria-current="true"
+                            aria-label="Slide 1"></button>
+                        <button type="button"
+                            data-bs-target="#carouselExampleCaptions"
+                            data-bs-slide-to="1"
+                            aria-label="Slide 2"></button>
+                        <button type="button"
+                            data-bs-target="#carouselExampleCaptions"
+                            data-bs-slide-to="2"
+                            aria-label="Slide 3"></button>
+                    </div>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="https://cdn2.unrealengine.com/egs-ac-mirage-carousel-desktop-1920x1080-b74b6e380b4d.jpg?h=720&quality=medium&resize=1&w=1280"
+                                class="d-block w-100"
+                                alt="...">
+                            <div
+                                class="carousel-caption d-none d-md-block banner_body">
+                                <div
+                                    class="banner_description">
+                                    <div
+                                        class="banner_brand">
+                                    </div>
+                                    <div
+                                        class="banner-head_description">
+                                        <span
+                                            class="banner_isAvailabel mb-2 mb-md-4">Now
+                                            Availabel</span>
+                                        <span
+                                            class="mb-2 mb-md-4 banner-text_wrapper">
+                                            <div
+                                                class="d-grid">
+                                                <div
+                                                    class="banner_text">
+                                                    In
+                                                    space,
+                                                    no one
+                                                    can hear
+                                                    you
+                                                    scream.
+                                                    But this
+                                                    is not
+                                                    space.
+                                                </div>
+                                            </div>
+                                        </span>
+                                    </div>
+                                    <div
+                                        class="banner_offer mt-4 w-100">
+                                        <div class="d-flex">
+                                            <a class="banner-buynow_offer"
+                                                href="/en-US/p/dead-by-daylight--alien-chapter-pack">Buy
+                                                Now</a>
+                                            <button
+                                                class="banner-addwishlist_offer"
+                                                aria-label="Add to Wishlist">
+                                                <span
+                                                    class="offer_text"><span>Add
+                                                        to
+                                                        Wishlist</span></span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://cdn2.unrealengine.com/egs-dead-by-daylight-ch29-carousel-desktop-1248x702-25b8bb141084.jpg?h=720&quality=medium&resize=1&w=1280"
+                                class="d-block w-100"
+                                alt="...">
+                            <div
+                                class="carousel-caption d-none d-md-block banner_body">
+                                <div
+                                    class="banner_description">
+                                    <div
+                                        class="banner_brand">
+                                    </div>
+                                    <div
+                                        class="banner-head_description">
+                                        <span
+                                            class="banner_isAvailabel mb-2 mb-md-4">Now
+                                            Availabel</span>
+                                        <span
+                                            class="mb-2 mb-md-4 banner-text_wrapper">
+                                            <div
+                                                class="d-grid">
+                                                <div
+                                                    class="banner_text">
+                                                    In
+                                                    space,
+                                                    no one
+                                                    can hear
+                                                    you
+                                                    scream.
+                                                    But this
+                                                    is not
+                                                    space.
+                                                </div>
+                                            </div>
+                                        </span>
+                                    </div>
+                                    <div
+                                        class="banner_offer mt-4 w-100">
+                                        <div class="d-flex">
+                                            <a class="banner-buynow_offer"
+                                                href="/en-US/p/dead-by-daylight--alien-chapter-pack">Buy
+                                                Now</a>
+                                            <button
+                                                class="banner-addwishlist_offer"
+                                                aria-label="Add to Wishlist">
+                                                <span
+                                                    class="offer_text"><span>Add
+                                                        to
+                                                        Wishlist</span></span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://cdn2.unrealengine.com/egs-ac-mirage-carousel-desktop-1920x1080-b74b6e380b4d.jpg?h=720&quality=medium&resize=1&w=1280"
+                                class="d-block w-100"
+                                alt="...">
+                            <div
+                                class="carousel-caption d-none d-md-block banner_body">
+                                <div
+                                    class="banner_description">
+                                    <div
+                                        class="banner_brand">
+                                    </div>
+                                    <div
+                                        class="banner-head_description">
+                                        <span
+                                            class="banner_isAvailabel mb-2 mb-md-4">Now
+                                            Availabel</span>
+                                        <span
+                                            class="mb-2 mb-md-4 banner-text_wrapper">
+                                            <div
+                                                class="d-grid">
+                                                <div
+                                                    class="banner_text">
+                                                    In
+                                                    space,
+                                                    no one
+                                                    can hear
+                                                    you
+                                                    scream.
+                                                    But this
+                                                    is not
+                                                    space.
+                                                </div>
+                                            </div>
+                                        </span>
+                                    </div>
+                                    <div
+                                        class="banner_offer mt-4 w-100">
+                                        <div class="d-flex">
+                                            <a class="banner-buynow_offer"
+                                                href="/en-US/p/dead-by-daylight--alien-chapter-pack">Buy
+                                                Now</a>
+                                            <button
+                                                class="banner-addwishlist_offer"
+                                                aria-label="Add to Wishlist">
+                                                <span
+                                                    class="offer_text"><span>Add
+                                                        to
+                                                        Wishlist</span></span>
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                            </div>
+                        </div>
+                        {{-- <div class="carousel-item">
+                            <img src="https://cdn2.unrealengine.com/egs-ac-mirage-carousel-desktop-1920x1080-b74b6e380b4d.jpg?h=720&quality=medium&resize=1&w=1280"
+                                class="d-block w-100"
+                                alt="...">
+                            <div
+                                class="carousel-caption d-none d-md-block">
+                                Hello
+
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="https://cdn2.unrealengine.com/egs-ac-mirage-carousel-desktop-1920x1080-b74b6e380b4d.jpg?h=720&quality=medium&resize=1&w=1280"
+                                class="d-block w-100"
+                                alt="...">
+                            <div
+                                class="carousel-caption d-none d-md-block">
+                                Hello
+
+                            </div>
+                        </div> --}}
+                    </div>
+                    <button
+                        class="carousel-control-prev d-none"
+                        type="button"
+                        data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide="prev">
+                        <span
+                            class="carousel-control-prev-icon"
+                            aria-hidden="true"></span>
+                        <span
+                            class="visually-hidden">Previous</span>
+                    </button>
+                    <button
+                        class="carousel-control-next d-none"
+                        type="button"
+                        data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide="next">
+                        <span
+                            class="carousel-control-next-icon"
+                            aria-hidden="true"></span>
+                        <span
+                            class="visually-hidden">Next</span>
+                    </button>
                 </div>
             </div>
 
@@ -38,10 +283,14 @@
                 <div class="product_wrapper text-white">
                     @foreach ($viewData['games'] as $game)
                         <div class="game_product_thumb">
-                            <div class="game_product_avatar">
-                                <img class="img-fluid" src="{{ '/storage/' . $game->getIamge() }}" alt="thumnail">
+                            <div
+                                class="game_product_avatar">
+                                <img class="img-fluid"
+                                    src="{{ '/storage/' . $game->getIamge() }}"
+                                    alt="thumnail">
                             </div>
-                            <span class="game_title truncated">
+                            <span
+                                class="game_title truncated">
                                 {{ $game->getNameGame() }}
                             </span>
                         </div>
