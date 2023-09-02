fa449c6cd5d7ea14f4898dd4f5589d3b1837353c
Thanh Loi <you@example.com>
2023-09-02 11:00:04 +0700

    search UI complete

--- resources/views/layouts/parts/header.blade.php
+++ resources/views/layouts/parts/header.blade.php
@@ -1,25 +1,35 @@
 <div class="container-fluid bg-dark text-white">
     <nav class="subnav d-flex align-items-center overflow-hidden">
-        <div class="container-fluid d-flex justify-content-between align-items-center">
+        <div
+            class="container-fluid d-flex justify-content-between align-items-center">
             <div class="brand_img">
-                <img class="img-fluid" src="{{ asset('/logo.svg') }}" alt="logo-img">
+                <img class="img-fluid" src="{{ asset('/logo.svg') }}"
+                    alt="logo-img">
             </div>
             <div class="flex-grow-1 web_options">
-                <a href="#" class="text-uppercase text-light active nav_options">store</a>
-                <a href="#" class="text-uppercase text-light nav_options">distribution</a>
-                <a href="#" class="text-uppercase text-light nav_options">game</a>
+                <a href="#"
+                    class="text-uppercase text-light active nav_options">store</a>
+                <a href="#"
+                    class="text-uppercase text-light nav_options">distribution</a>
+                <a href="#"
+                    class="text-uppercase text-light nav_options">game</a>
             </div>
             <div class="nav_controls d-flex">
                 @guest
-                    <a href="{{ route('login') }}" class="text-light nav_options me-2"><i
+                    <a href="{{ route('login') }}"
+                        class="text-light nav_options me-2"><i
                             class="fa-solid fa-right-to-bracket me-2"></i>
                         Login</a>
-                    <a href="{{ route('register') }}" class="text-light nav_options">Register</a>
+                    <a href="{{ route('register') }}"
+                        class="text-light nav_options">Register</a>
                 @else
-                    <a href="{{ route('cart.orders') }}" class="text-light nav_options">My
+                    <a href="{{ route('cart.orders') }}"
+                        class="text-light nav_options">My
                         Order</a>
-                    <a href="{{ route('cart.index') }}" class="text-light nav_options">Cart</a>
-                    <form action="{{ route('logout') }}" id="logout" method="POST">
+                    <a href="{{ route('cart.index') }}"
+                        class="text-light nav_options">Cart</a>
+                    <form action="{{ route('logout') }}" id="logout"
+                        method="POST">
                         <a role="button" class="text-light nav_options"
                             onclick="document.getElementById('logout').submit()">
                             Logout
@@ -35,12 +45,20 @@
 <div class="container-fluid bg-black text-white">
     <div class="headnav">
         <div class="container-md">
-            <div class="wrapper_headnav py-5 d-flex justify-content-start align-items-center">
-                <div class="form-group headnav_search">
-                    <span><i class="fa-solid fa-magnifying-glass"></i></span>
-                    <input type="text" id="searchGame" name="gameName" placeholder="Search store" />
+            <div
+                class="wrapper_headnav py-5 d-flex justify-content-start align-items-center">
+                <div class="search_area">
+                    <div class="form-group headnav_search">
+                        <span><i
+                                class="fa-solid fa-magnifying-glass"></i></span>
+                        <input type="text" id="searchGame" name="gameName"
+                            placeholder="Search store" />
+                    </div>
+                    <div class="search_results bottom-0 left-0 right-0">
+                        <div id="ListGames"></div>
+                    </div>
                 </div>
-                <div id="ListGames"></div>
+
                 <div class="ms-3">
                     <a href="#"
                         class="d-inline-block nav-menu_item active h3 bg-transparent text-capitalize d-inline-block me-3">
@@ -55,18 +73,22 @@
             </div>
         </div>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js">
+    </script>
     <script>
+        // Query search result
         $(document).ready(function() {
             $('#searchGame').keyup(function() {
                 var query = $(this).val();
                 if (query != '') {
                     $.ajax({
-                        url: `{{ route('search') }}?key=` + query,
+                        url: `{{ route('search') }}?key=` +
+                            query,
 
                         method: "GET",
                         success: function(data) {
-                            $('#ListGames').show(300000);
+                            $('#ListGames').show(
+                                300000);
                             $('#ListGames').html(data);
                         }
 
@@ -77,4 +99,17 @@
                 }
             });
         });
+
+        // Active, not active search result
+        const searchInput = document.getElementById('searchGame');
+        const searchResult = document.querySelector('.search_results')
+        searchInput.addEventListener('focus', () => {
+            console.log("Hi")
+            searchResult.classList.add('active')
+        });
+
+        searchInput.addEventListener('blur', () => {
+            console.log("Hi")
+            searchResult.classList.remove('active')
+        })
     </script>
--- resources/views/layouts/search.blade.php
+++ resources/views/layouts/search.blade.php
@@ -1,10 +1,22 @@
-@foreach ($viewData['searchGame'] as $search)
-    <div class="row">
-        <div class="col-4">
-            <img src="{{ '/storage/' . $search->getIamge() }}" alt="" class="img-fluid">
-        </div>
-        <div class="col">
-            <a href="{{ route('clients.gamesDetail', ['id' => $search->getGameId()]) }}"> {{ $search->getNameGame() }}</a>
-        </div>
-    </div>
-@endforeach
+<div
+    class="search_result_title bg-dark border-bottom border-secondary text-light">
+    {{ count($viewData['searchGame']) }} games found</div>
+<div class="search_result_wrapper text-white bg-dark">
+    @foreach ($viewData['searchGame'] as $search)
+        <a href="{{ route('clients.gamesDetail', ['id' => $search->getGameId()]) }}"
+            class="game_product_thumb">
+            <div class="game_product_avatar">
+                <div>
+                    <div class="d-flex align-items-center overflow-hidden">
+                        <img class="img-fluid"
+                            src="{{ '/storage/' . $search->getIamge() }}"
+                            alt="thumnail">
+                    </div>
+                </div>
+            </div>
+            <span class="game_title truncated text-light">
+                {{ $search->getNameGame() }}
+            </span>
+        </a>
+    @endforeach
+</div>
